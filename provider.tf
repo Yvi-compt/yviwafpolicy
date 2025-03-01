2539cd40a0c1d43022c156b5cd0e282b4b16b2fa
Yvi-compt <yvesplacide@gmail.com>
2025-02-28 19:36:44 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,7 @@ terraform{
 
 provider "azurerm"{
   features{}
-  skip_provider_registration="true"
+ # skip_provider_registration="true"
   
   subscription_id=var.subscription_id
   client_id=var.client_id
